0cf845dca16eccc4fcae2e1b12b2747df3521c2d
Sergii <rumabasy@gmail.com>
2022-09-22 01:37:40 +0300

    add get_role_by_email

--- handler_login.php
+++ handler_login.php
@@ -11,4 +11,8 @@ $remember = $_POST['remember'];
 
 $pdo = new PDO("mysql:host=localhost;dbname=marlin","root" , "");
 
-if(login($email, $password)) redirect_to('users.php');
+if(login($email, $password)){
+    $_SESSION['id']=get_id_by_email($email);
+    $_SESSION['role']=get_role_by_email($email);
+    redirect_to('users.php');
+} 
