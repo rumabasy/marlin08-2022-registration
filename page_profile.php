fc2b62b2fd718794d8f6dbf4151607d786f56853
Sergii <rumabasy@gmail.com>
2022-09-23 22:01:54 +0300

    made page_profile

--- page_profile.php
+++ page_profile.php
@@ -2,14 +2,15 @@
 session_start(); 
 require 'my_function.php';
 $common=get_common_infa_by_id($_GET['id']);
-dump($common,6);
-// echo $status=get_status_by_id_user($_common);
+// dump($common,6);
+// echo $common['id_user'];
+// $status=get_status_by_id_user($common);
 // dump($status,6);
 // dump($status);
-// $media=get_avatar_by_id_user($_common);
+$media=get_avatar_by_id_user($common);
 // dump($media,6);
-$socials=get_socials_by_id($_common);
-dump($socials);
+$socials=get_socials_by_id($common);
+// dump($socials);
 
 ?>
 <!DOCTYPE html>
@@ -49,7 +50,7 @@ dump($socials);
         <main id="js-page-content" role="main" class="page-content mt-3">
             <div class="subheader">
                 <h1 class="subheader-title">
-                    <i class='subheader-icon fal fa-user'></i> Иван Иванов
+                    <i class='subheader-icon fal fa-user'></i> <?php echo $common['name'] ?>
                 </h1>
             </div>
             <div class="row">
@@ -59,19 +60,19 @@ dump($socials);
                         <div class="row no-gutters row-grid">
                             <div class="col-12">
                                 <div class="d-flex flex-column align-items-center justify-content-center p-4">
-                                    <img src="img/demo/avatars/avatar-admin-lg.png" class="rounded-circle shadow-2 img-thumbnail" alt="">
+                                    <img src="uploads/<?php echo $media ?>" class="rounded-circle shadow-2 img-thumbnail" alt="">
                                     <h5 class="mb-0 fw-700 text-center mt-3">
-                                        Иван Иванов 
-                                        <small class="text-muted mb-0">Toronto, Canada</small>
+                                        <?php echo $common['name'] ?>
+                                        <small class="text-muted mb-0"><?php echo $common['work_space'] ?></small>
                                     </h5>
                                     <div class="mt-4 text-center demo">
-                                        <a href="javascript:void(0);" class="fs-xl" style="color:#C13584">
+                                        <a href="https://hipolink.me/example-<?php echo $socials['inst']?>" class="fs-xl" style="color:#C13584">
                                             <i class="fab fa-instagram"></i>
                                         </a>
-                                        <a href="javascript:void(0);" class="fs-xl" style="color:#4680C2">
+                                        <a href="https://id.vk.com/auth?app_id=<?php echo $socials['vk']?>" class="fs-xl" style="color:#4680C2">
                                             <i class="fab fa-vk"></i>
                                         </a>
-                                        <a href="javascript:void(0);" class="fs-xl" style="color:#0088cc">
+                                        <a href="https://web.telegram.org/z/#-<?php echo $socials['tg']?>" class="fs-xl" style="color:#0088cc">
                                             <i class="fab fa-telegram"></i>
                                         </a>
                                     </div>
@@ -79,12 +80,12 @@ dump($socials);
                             </div>
                             <div class="col-12">
                                 <div class="p-3 text-center">
-                                    <a href="tel:+13174562564" class="mt-1 d-block fs-sm fw-400 text-dark">
-                                        <i class="fas fa-mobile-alt text-muted mr-2"></i> +1 317-456-2564</a>
-                                    <a href="mailto:oliver.kopyov@marlin.ru" class="mt-1 d-block fs-sm fw-400 text-dark">
-                                        <i class="fas fa-mouse-pointer text-muted mr-2"></i> oliver.kopyov@marlin.ru</a>
+                                    <a href="tel:<?php echo $common['phone'] ?>" class="mt-1 d-block fs-sm fw-400 text-dark">
+                                        <i class="fas fa-mobile-alt text-muted mr-2"></i> <?php echo $common['phone'] ?></a>
+                                    <a href="mailto:<?php echo $common['email'] ?>" class="mt-1 d-block fs-sm fw-400 text-dark">
+                                        <i class="fas fa-mouse-pointer text-muted mr-2"></i> <?php echo $common['email'] ?></a>
                                     <address class="fs-sm fw-400 mt-4 text-muted">
-                                        <i class="fas fa-map-pin mr-2"></i> Восточные Королевства, Штормград 15
+                                        <i class="fas fa-map-pin mr-2"></i> <?php echo $common['address'] ?>
                                     </address>
                                 </div>
                             </div>
