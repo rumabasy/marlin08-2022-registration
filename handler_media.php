77eb22bd0de8d39b49f2ada82a0b018a0f13a499
Sergii <rumabasy@gmail.com>
2022-09-27 00:33:28 +0300

    add success message if ok

--- handler_media.php
+++ handler_media.php
@@ -8,6 +8,6 @@ $old_name = get_avatar_by_id_user2($id);
 delete_old_avatar_from_uploads($old_name);
 // exit;
 edit_avatar_into_media($_FILES, $id);
-set_sess_mess('sussess','АВАТАР изменен');
+set_sess_mess('success','АВАТАР изменен');
 redirect_to('users.php');
 exit;    
