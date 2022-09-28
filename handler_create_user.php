2ece4ddd49864c71de1591dca7fd704b1e7e395a
Sergii <rumabasy@gmail.com>
2022-09-28 16:51:42 +0300

    start change handler_create_user.php

--- handler_create_user.php
+++ handler_create_user.php
@@ -5,13 +5,20 @@ require "my_function.php";
 // dump($_POST,5);
 // dump($_FILES,4);
 
-$pdo = new PDO("mysql:host=localhost;dbname=marlin", "root", "");
-
-if (get_id_by_email($_POST['email'])){
-    $_SESSION['danger']="такой email уже занят ";
-    redirect_to('create_user.php');
-    exit;
-} else{ 
+if(login($_POST['email'],$_POST['password'])){
+    //change user
+    $id=get_id_by_email($_POST['email'];
+    if($_POST[])
+    edit_common_infa_by_id($_POST, $id);
+    edit_avatar_into_media($_FILES, $id);
+    edit_status($id, $_POST);
+    edit_socials_by_id($_POST, $id);
+} else{
+    if (get_id_by_email($_POST['email'])){
+        set_sess_mess('danger',"такой email уже занят, <br>Данные изменить можно если знаешь пароль<br>");
+        redirect_to('create_user.php');
+        exit;
+    } else { 
     add_user($_POST['email'],$_POST['password']);
     $_SESSION['id'] = get_id_by_email($_POST['email']);
     save_into_common_infa($_POST);
